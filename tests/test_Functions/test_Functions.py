1cef44da492cde2663e785c02d8a594cc12f9560
ChrisPoul <>
2021-01-04 15:46:17 -0600

    Created more tests and fixed minor errors

--- tests/test_Functions/test_Functions.py
+++ tests/test_Functions/test_Functions.py
@@ -1,15 +1,18 @@
-from AASAdministrator.Functions import format_price, add_iva, convert_to_pesos
-from AASAdministrator.Functions import some_filter
+from AASAdministrator.Functions import format_price, add_iva, convert_to_pesos, get_correct_name, get_name_list
+from AASAdministrator.Functions import get_repeated_characters, get_unique_characters, get_correct_object
+
+
+def test_get_name_list():
+    dictionary_list = [{"name":"Shelly 1", "code": "SHELLY1"}, {"name":"Shelly 1 PM", "code": "SHELLY1PM"}]
+
+    assert get_name_list(dictionary_list) == ["Shelly 1", "Shelly 1 PM"]
 
 
 def test_format_price():
-    price1 = 10.0
-    price2 = 58.49
-    price3 = 7291.5
-
-    assert format_price(price1) == "$10.00"
-    assert format_price(price2) == "$58.49"
-    assert format_price(price3) == "$7291.5"
+    assert format_price(10.0) == "$10.00"
+    assert format_price(58.49) == "$58.49"
+    assert format_price(7291.5) == "$7291.50"
+    assert format_price(1) == "$1.00"
 
 
 def test_add_iva():
@@ -24,11 +27,31 @@ def test_convert_to_pesos():
     dollars1 = 1.0
     dollars2 = 48.0
 
-    assert convert_to_pesos(dollars1) == "$19.88"
-    assert convert_to_pesos(dollars2) == "$954.24"
+    assert convert_to_pesos(dollars1) == 19.88
+    assert convert_to_pesos(dollars2) == 954.24
 
 
-def test_some_filter():
-    user_input = "Shelly 1 PM"
+def test_get_correct_name():
+    name_list = ["Shelly 1", "Shelly 1 PM", "Shelly 1L"]
 
-    assert some_filter(user_input) == "Shelly 1 PM"
+    assert get_correct_name("Shelly 1 PM bla bla", name_list) == "Shelly 1 PM"
+    assert get_correct_name("some random thing", name_list) == ""
+
+
+def test_get_correct_object():
+    dictionary_list = [{"name":"Shelly 1", "code": "SHELLY1"}, {"name":"Shelly 1 PM", "code": "SHELLY1PM"}]
+
+    assert get_correct_object("Shelly 1 PM op", dictionary_list) == dictionary_list[1]
+    assert get_correct_object("Shelly 1 23d 21d", dictionary_list) == dictionary_list[0]
+
+
+def test_get_repeated_characters():
+    names_list = ["Shelly 1", "Shelly 1 PM", "Shelly Plug"]
+
+    assert get_repeated_characters(names_list) == "Shelly "
+
+
+def test_get_unique_characters():
+    names_list = ["Shelly 1", "Shelly 1 PM", "Shelly Plug"]
+
+    assert get_unique_characters(names_list) == ["1", "1 PM", "Plug"]
