9c8103b361a7c855adebc16309946e72ac976fca
ChrisPoul <>
2021-01-03 20:09:20 -0600

    Moved functions to another file

--- AASAdministrator/Base_Item.py
+++ AASAdministrator/Base_Item.py
@@ -1,30 +1,5 @@
 from textwrap import dedent
-
-
-def format_price(price):
-    if price[-2:] == ".0":
-        price += "0"
-
-        return f"${price}"
-
-
-
-def add_iva(price):
-    price_with_iva = str(round(price * 1.16, 2))
-
-    return format_price(price_with_iva)
-
-
-
-def convert_to_pesos(price):
-    current_dollar_value = 19.88
-    price_in_pesos = str(round(price * current_dollar_value))
-
-    return format_price(price_in_pesos)
-
-
-def get_total(items):
-    pass
+from Functions import format_price, add_iva, convert_to_pesos
 
 
 class Item:
