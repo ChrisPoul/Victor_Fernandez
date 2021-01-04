7f0b0574fa3ac74647844410b3289cb45858ebaa
ChrisPoul <>
2021-01-04 15:52:37 -0600

    Cleaned structure and abstracted some functions

--- AASAdministrator/UI.py
+++ AASAdministrator/UI.py
@@ -1,48 +1,41 @@
 from os import system
-from Shelly import Shelly_1, Shelly_1L, Shelly_HC7, Shelly_Plug, Shelly_RGBW2
-from Shelly import Shelly_Dimmer, Shelly_25, Shelly_1_PM, Shelly_HT
+from Product_Catalog import Product_Catalog
+from Functions import get_correct_object, get_correct_name, format_price, add_iva, convert_to_pesos
 
-shelly_1 = Shelly_1()
-shelly_plug = Shelly_Plug()
-shelly_dimmer = Shelly_Dimmer()
-shelly_25 = Shelly_25()
-shelly_1_pm = Shelly_1_PM()
-shelly_ht = Shelly_HT()
-shelly_rgbw2 = Shelly_RGBW2()
-shelly_1l = Shelly_1L()
-shelly_hc7 = Shelly_HC7()
+product_catalog = Product_Catalog()
 
-shellies = [
-    shelly_1, shelly_1l, shelly_hc7,
-    shelly_plug, shelly_dimmer, shelly_25,
-    shelly_1_pm, shelly_ht, shelly_rgbw2
-    ]
 stop_signal = 0
 system("clear")
-
 while stop_signal == 0:
     user_input = input('Type "exit" to exit\n')
-
     system("clear")
 
     if user_input == "exit":
         stop_signal = 1
 
-    for shelly in shellies:
-        my_dict = vars(shelly)
-        if user_input in my_dict:
-            print(my_dict[user_input])
+    elif user_input.startswith("Sum"):
+        my_total, sell_total = product_catalog.sum_mode()
+        print(f"Mi total: {format_price(my_total)}")
+        print(f"Mi total con IVA: {add_iva(my_total)}")
+        print(f"Total de venta: {format_price(sell_total)}")
+        print(f"Total de venta con IVA: {add_iva(sell_total)} USD o {add_iva(convert_to_pesos(sell_total))} MXN")
 
-        elif my_dict["name"] + " " in user_input:
-            numbers = "1234567890"
-            user_input = user_input[user_input.find("Add"):]
-            if "Add" in user_input:
-                to_add = ""
-                for char in user_input:
-                    if char in numbers:
-                        to_add += char
-            total = my_dict["customer_price"] * int(to_add)
-            print(to_add)
-            print(f"total {total}")
+    products_list = product_catalog.product_catalog
+    names_list = [product["name"] for product in products_list]
 
-            print(my_dict["code"])
+    for product in products_list:
+        product_name = product["name"]
+
+        if user_input in product:
+            print(product[user_input])
+
+        if product_name == get_correct_name(user_input, names_list):
+            correct_product = get_correct_object(user_input, products_list)
+
+            for attribute in correct_product:
+                if attribute in user_input:
+                    print(correct_product[attribute])
+
+            if product_name == user_input:
+                for attribute in correct_product:
+                    print(f"{attribute} : {correct_product[attribute]}")
