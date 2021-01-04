a0068c4a7edae62561e1295627fa4b1b4f1bc7d2
ChrisPoul <>
2021-01-03 21:04:48 -0600

    Fixed some bugs and introduced new function

--- AASAdministrator/Functions.py
+++ AASAdministrator/Functions.py
@@ -1,20 +1,24 @@
 
+def some_filter(thing):
+    pass
+
 
 def format_price(price):
+    price = str(price)
     if price[-2:] == ".0":
         price += "0"
 
-        return f"${price}"
+    return f"${price}"
 
 
 def add_iva(price):
-    price_with_iva = str(round(price * 1.16, 2))
+    price_with_iva = round(price * 1.16, 2)
 
     return format_price(price_with_iva)
 
 
 def convert_to_pesos(price):
     current_dollar_value = 19.88
-    price_in_pesos = str(round(price * current_dollar_value))
+    price_in_pesos = round(price * current_dollar_value, 2)
 
     return format_price(price_in_pesos)
