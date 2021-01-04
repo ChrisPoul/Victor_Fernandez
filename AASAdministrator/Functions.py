142e26cab33c3bb84757f5eb6fe7a5d9c214173c
ChrisPoul <>
2021-01-03 20:08:44 -0600

    File to store general functions

---
+++ AASAdministrator/Functions.py
@@ -0,0 +1,20 @@
+
+
+def format_price(price):
+    if price[-2:] == ".0":
+        price += "0"
+
+        return f"${price}"
+
+
+def add_iva(price):
+    price_with_iva = str(round(price * 1.16, 2))
+
+    return format_price(price_with_iva)
+
+
+def convert_to_pesos(price):
+    current_dollar_value = 19.88
+    price_in_pesos = str(round(price * current_dollar_value))
+
+    return format_price(price_in_pesos)
