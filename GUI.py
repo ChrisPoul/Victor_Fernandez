a6ad0c043e3ead434938394a4d4669145b9a4a3e
ChrisPoul <>
2021-01-05 19:06:52 -0600

    Made a functional design for dysplaing information

--- GUI.py
+++ GUI.py
@@ -5,63 +5,53 @@ product_catalog = Product_Catalog()
 
 window = tk.Tk()
 
-top_frm = tk.Frame(
-    master=window,
-    relief=tk.GROOVE,
-    borderwidth=2
-)
-top_frm.pack(fill=tk.X, expand=True,)
+first_product = product_catalog.products_list[0]
+first_product["amnt."] = 0
+first_product["Total"] = 0
 
-for key in product_catalog.products_list[0]:
+for i, key in enumerate(first_product):
+    window.rowconfigure(0, weight=1, minsize=50)
+    window.columnconfigure(i, weight=1, minsize=50)
+
     key_frame = tk.Frame(
-        master=top_frm,
-        relief=tk.RIDGE,
+        master=window,
+        relief=tk.RAISED,
         borderwidth=1
     )
-    key_frame.pack(fill=tk.X, expand=True, side=tk.LEFT)
+    key_frame.grid(row=0, column=i, padx=2, pady=5, sticky="ews")
 
     key_lbl = tk.Label(
         master=key_frame,
         text=key,
-        height=1
+        height=2
     )
-    key_lbl.pack(fill=tk.X, expand=True, padx=5, pady=5)
-
-bottom_frm = tk.Frame(
-    master=window,
-    borderwidth=1
-)
-bottom_frm.pack(fill=tk.BOTH, expand=True)
+    key_lbl.pack(fill=tk.X, expand=True, side=tk.BOTTOM)
 
 for i, product in enumerate(product_catalog.products_list):
     product["amnt."] = 0
     product["Total"] = 0
 
-    bottom_frm.rowconfigure(i, weight=1, minsize=50)
+    window.rowconfigure(i+1, weight=1, minsize=50)
 
     for j, key in enumerate(product):
-        bottom_frm.columnconfigure(j, weight=1, minsize=50)
-
-        main_frame = tk.Frame(
-            master=bottom_frm,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
-        main_frame.grid(row=i, column=j, padx=2, pady=2, sticky="nsew")
+        window.columnconfigure(j, weight=1, minsize=50)
 
         frm_value = tk.Frame(
-            master=main_frame,
+            master=window,
             relief=tk.SUNKEN,
+            height=4,
             borderwidth=1
         )
-        frm_value.pack(fill=tk.X, expand=True)
+        frm_value.grid(row=i+1, column=j, padx=2, pady=2, sticky="nsew")
 
         lbl_value = tk.Label(
             master=frm_value,
             text=product[key],
-            height=5
+            relief=tk.GROOVE,
+            wraplength=400,
+            height=3
         )
-        lbl_value.pack(fill=tk.X, expand=True)
+        lbl_value.pack(fill=tk.BOTH, expand=True, padx=1, pady=1)
 
 
 window.mainloop()
