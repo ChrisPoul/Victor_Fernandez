5188df73eac21d5c1f6b548332964e68411ea008
ChrisPoul <>
2021-01-05 23:03:37 -0600

    Refactored to only show the product values that we choose, these values can be easily changed

--- GUI.py
+++ GUI.py
@@ -1,14 +1,26 @@
 import tkinter as tk
 from Product_Catalog import Product_Catalog
-from Functions import format_price, add_iva
+from Functions import format_price, add_iva, get_product_total
 
 product_catalog = Product_Catalog()
 
 window = tk.Tk()
 
-spanish_keys = ["Codigo", "Nombre", "Descripción", "Marca", "Imagen", "Mi precio", "Mi precio mas iva", "Precio de Venta", "Precio Venta mas iva", "amnt.", "Total"]
+wanted_names = {
+    "code": "Codigo",
+    "name": "Nombre",
+    "description": "Descripción",
+    "brand": "Marca",
+    "image": "Imagen",
+    "my_price": "Mi precio",
+    "my_price_iva": "Mi precio mas iva",
+    "sell_price": "Precio de Venta",
+    "sell_price_iva": "Precio Venta mas iva",
+    "amnt.": "Cantidad",
+    "Total": "Total"
+}
 
-for i, key in enumerate(spanish_keys):
+for i, key in enumerate(wanted_names):
     window.rowconfigure(0, weight=1, minsize=50)
     window.columnconfigure(i, weight=1, minsize=50)
 
@@ -21,7 +33,7 @@ for i, key in enumerate(spanish_keys):
 
     key_lbl = tk.Label(
         master=key_frame,
-        text=key,
+        text=wanted_names[key],
         height=2
     )
     key_lbl.pack(fill=tk.X, expand=True, side=tk.BOTTOM)
@@ -32,7 +44,7 @@ for i, product in enumerate(product_catalog.products_list):
 
     window.rowconfigure(i+1, weight=1, minsize=50)
 
-    for j, key in enumerate(product):
+    for j, key in enumerate(wanted_names):
         window.columnconfigure(j, weight=1, minsize=50)
         value = product[key]
         my_wrap_length = 400
@@ -53,15 +65,31 @@ for i, product in enumerate(product_catalog.products_list):
         )
         frm_value.grid(row=i+1, column=j, padx=2, pady=2, sticky="nsew")
 
-        lbl_value = tk.Label(
-            master=frm_value,
-            text=value,
-            relief=tk.GROOVE,
-            wraplength=my_wrap_length,
-            justify=my_justify,
-            height=3
-        )
-        lbl_value.pack(fill=tk.BOTH, expand=True, padx=1, pady=1)
+        if key == "amnt.":
+            ent_amnt = tk.Entry(
+                master=frm_value,
+                justify="center"
+            )
+            ent_amnt.pack(fill=tk.BOTH, expand=True, padx=1, pady=1)
+
+        else:
+            if key == "Total":
+                try:
+                    cuantity = int(ent_amnt.get())
+                    value = get_product_total(product, cuantity)
+
+                except ValueError:
+                    pass
+
+            lbl_value = tk.Label(
+                master=frm_value,
+                text=value,
+                relief=tk.GROOVE,
+                wraplength=my_wrap_length,
+                justify=my_justify,
+                height=3
+            )
+            lbl_value.pack(fill=tk.BOTH, expand=True, padx=1, pady=1)
 
 
 window.mainloop()
