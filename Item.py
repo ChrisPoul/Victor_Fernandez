68da49d5a7d8a86b23e95f2adcaff5fa6cf936e1
ChrisPoul <>
2021-01-05 23:00:43 -0600

    Added group and product line to Item and made prices_with_iva attributes created with the object

--- Item.py
+++ Item.py
@@ -5,15 +5,17 @@ from Functions import format_price, add_iva, convert_to_pesos
 class Item:
 
     def __init__(self, values):
-        code, name, description, brand, image, my_price, sell_price = values
+        group, line, code, name, description, brand, image, my_price, sell_price = values
 
+        self.group = group
+        self.line = line
         self.code = code
         self.name = name
-        self.description = dedent(description)
+        self.description = description
         self.brand = brand
         self.image = image
 
         self.my_price = float(my_price)
-        self.my_price_iva = add_iva(my_price)
+        self.my_price_iva = add_iva(self.my_price)
         self.sell_price = float(sell_price)
-        self.sell_price_iva = add_iva(sell_price)
+        self.sell_price_iva = add_iva(self.sell_price)
