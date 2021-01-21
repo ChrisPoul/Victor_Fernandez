3c2fdd68ac7a2062a1dafb5b10c97bb41a96c1bd
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-01-20 18:47:34 -0600

    Now the receipt fills product information with it's code

--- second-draft/VicSM/receipt.py
+++ second-draft/VicSM/receipt.py
@@ -1,16 +1,33 @@
 from flask import (
-    Blueprint, g, render_template
+    Blueprint, g, render_template, request
 )
+from VicSM.inventory import get_product
 
 bp = Blueprint('receipt', __name__)
 
 heads = [
     "codigo", "nombre", "descripcion", "marca", 
-    "imagen", "mi_precio", "precio_venta", "inventario"
+    "imagen", "precio_venta", "cantidad", "total"
     ]
+products = []
 
 
 @bp.route('/receipt', methods=('GET', 'POST'))
 def receipt():
+    empty_product = {}
 
-    return render_template('receipt/receipt.html', heads=heads)
+    for head in heads:
+        empty_product[head] = ""
+
+
+    if request.method == "POST":
+        codigo = request.form["codigo"]
+        try:
+            cantidad = request.form["cantidad"]
+        except KeyError:
+            pass
+
+        product = get_product(codigo)
+        products.append(product)
+
+    return render_template('receipt/receipt.html', heads=heads, products=products, empty_product=empty_product)
