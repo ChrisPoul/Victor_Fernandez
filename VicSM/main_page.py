1f9b04976bea72a944fca84adb3d52cd347d3b0a
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-02-11 11:02:47 -0600

    Now the main page looks much better and shows some info

--- VicSM/main_page.py
+++ VicSM/main_page.py
@@ -1,11 +1,35 @@
 from flask import (
     Blueprint, request, render_template
 )
+from VicSM.db import get_receipts
+from VicSM.client import get_client
+from VicSM.inventory import get_product
 
 bp = Blueprint('main_page', __name__)
 
 
 @bp.route('/')
 def main_page():
+    receipts = get_receipts()
+    recent_receipts = {}
+    clients = []
+    for client_id in receipts:
+        client_receipts = receipts[client_id]
+        recent_receipt_id = client_receipts["numero_de_recibos"]
+        recent_receipt = client_receipts[str(recent_receipt_id)]
+        client = get_client(client_id)
+        clients.append(client)
+        client_name = client["nombre"]
+        recent_receipts[client_name] = recent_receipt
 
-    return render_template('main_page/main_page.html')
+    codigos_frecuentes = ["SHELLY_1", "SHELLY_DIMMER2", "SHELLY_1PM"]
+    products = {}
+    for codigo in codigos_frecuentes:
+        product = get_product(codigo)
+        products[codigo] = product
+
+
+    return render_template(
+        'main_page/main_page.html', receipts=recent_receipts, clients=clients,
+        products=products
+        )
