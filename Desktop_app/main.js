bd09244ff0fc0ee3d8683181271e37e85b26a6bb
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-02-13 11:18:47 -0600

    Now the application saves a pdf file of the view when pressing Ctrl+P, still it does not work as intended so it needs more work

--- Desktop_app/main.js
+++ Desktop_app/main.js
@@ -1,26 +1,59 @@
 const {app, BrowserWindow} = require('electron');
-
-function createWindow() {
-    window = new BrowserWindow();
-    window.loadFile('index.html')
-};
-
-app.on('ready', createWindow)
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
 
 let {PythonShell} = require('python-shell');
-const path = require('path');
 
 var options = {
     scriptPath : __dirname,
     args: []
 };
 
-let pyshell = new PythonShell('run_app.py', options)
-
+var pyshell = new PythonShell('run_app.py', options)
 pyshell.on('message', function(message) {
     console.log(message);
-    console.log(typeof message);
 });
+
+function print_window(window) {
+    // Use default printing options
+    window.webContents.printToPDF({}).then(data => {
+        let file_name = window.webContents.getURL();
+        const pdfPath = path.join(os.homedir(), 'Desktop', 'temp.pdf')
+        fs.writeFile(pdfPath, data, (error) => {
+        if (error) throw error
+        console.log(`Wrote PDF successfully to ${pdfPath}`)
+        })
+    }).catch(error => {
+        console.log(`Failed to write PDF to ${pdfPath}: `, error)
+    })
+}
+
+function createWindow() {
+    window = new BrowserWindow();
+    setTimeout(() => { 
+         window.loadURL('http://127.0.0.1:5000/'); 
+        }, 150);
+};
+
+const { Menu, MenuItem } = require('electron')
+
+const menu = new Menu()
+menu.append(new MenuItem({
+  label: 'Print',
+  submenu: [{
+    role: 'print',
+    accelerator: process.platform === 'linux' ? 'Ctrl+P' : 'Ctrl+P',
+    click: () => { 
+        window = BrowserWindow.getFocusedWindow();
+        print_window(window)
+     }
+  }]
+}))
+
+Menu.setApplicationMenu(menu)
+
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
     app.quit()
